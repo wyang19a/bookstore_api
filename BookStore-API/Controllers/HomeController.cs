f392393a869f94ea54a91d008f46ce7507166b81
Wootae Yang <wyang19a@gmail.com>
2020-06-13 12:26:35 -0400

    initial commit

---
+++ BookStore-API/Controllers/HomeController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookStore_API.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BookStore_API.Controllers
+{
+    /// <summary>
+    /// Controllerrrr
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        // SERVICE INJECTION (ADD)
+        private readonly ILoggerService _logger;
+        // declare constructor for logger
+        public HomeController(ILoggerService logger)
+        {
+            _logger = logger;
+        }// now that service injection is complete, it can interact with requests below.
+
+        /// <summary>
+        /// GETTTT
+        /// </summary>
+        /// <returns></returns>
+        // GET: api/<HomeController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            _logger.LogInfo("Accessed Home Controller");
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<HomeController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            _logger.LogDebug("Got A Value");
+            return "value";
+        }
+
+        // POST api/<HomeController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+            _logger.LogError("This is an error");
+        }
+
+        // PUT api/<HomeController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+
+        }
+
+        // DELETE api/<HomeController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            _logger.LogWarn("This is a Warning.");
+        }
+    }
+}
---
+++ BookStore-API/Startup.cs
@@ -0,0 +1,119 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
+using BookStore_API.Data;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
+using System.IO;
+using BookStore_API.Contracts;
+using BookStore_API.Services;
+
+namespace BookStore_API
+{
+
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(
+                    Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            // ADD CORS, build policy
+            services.AddCors(o => {
+                o.AddPolicy("CorsPolicy", 
+                    builder => builder.AllowAnyOrigin()
+                                      .AllowAnyMethod()
+                                      .AllowAnyHeader());
+            }); // now go down to Configure (line 78) 
+
+
+
+            // add swagger services added from nuget package manager
+            services.AddSwaggerGen(c => { // lambda expression c, "token".
+                c.SwaggerDoc("v1", 
+                    new OpenApiInfo { 
+                        Title = "Book Store API", 
+                        Version = "v1" ,
+                        Description = "This is an educational API for a Book Store"
+                    });// 2 params, giving it "version1" name, new OpenApiInfo 
+
+                // below line getting the path where this project sits, and find xml
+                var xfile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml"; // $ > interpolation?
+                // getting path to xml file
+                var xpath = Path.Combine(AppContext.BaseDirectory, xfile);
+                // include xml/comments in documentation when swagger runs.
+                // refer to ///<Comments> in HomeController.cs
+                c.IncludeXmlComments(xpath);
+            });
+
+            // initialize NLog
+            services.AddSingleton<ILoggerService, LoggerService>();
+
+            //services.AddRazorPages();
+            services.AddControllers(); // leave it for last because everything else should be ready before adding controllers `ideally`
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            // app, when you are about to run, use Swagger.
+            app.UseSwagger();
+
+            // and use swagger UI.
+            app.UseSwaggerUI(c => {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Book Store API");
+                // display when route is / instead (start up page)
+                c.RoutePrefix = "";
+            });
+
+            app.UseHttpsRedirection();
+            //app.UseStaticFiles(); // such as css, js inside www
+
+            //USE CORS
+            app.UseCors("CorsPolicy"); //Ready for global interaction.
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                //endpoints.MapRazorPages();
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
