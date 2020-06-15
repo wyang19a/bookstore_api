70f7841b0119302dd13cf887165d9a8c95a9a5e3
wyang19a <wyang19a@gmail.com>
2020-06-15 14:09:20 -0400

    Setup HTTP Client and Services Repository

---
+++ BookStore-UI_server/Static/Endpoints.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore_UI_server.Static
+{
+    public static class Endpoints
+    {
+        public static string BaseUrl = "http://localhost:44392/";
+        public static string AuthorsEndpoint = $"{BaseUrl}api/authors/";
+        public static string BooksEndpoint = $"{BaseUrl}api/books/";
+    }
+}
